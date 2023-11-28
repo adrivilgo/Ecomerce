bef46e2462190cae5248e11e2e05a1988639e8f8
adrivilgo <elnegritosamuel@hotmail.com>
2023-11-28 10:19:06 -0500

    mi ecomerce

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import {CardComponent} from './card/card.component';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, CardComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'e-comers';
+}
